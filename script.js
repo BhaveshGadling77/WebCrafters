2e9c35b00eaf97ea613e9bd68146c74b0e3be6cf
Aditya-M-Kumbhar <adityamkumbharofficial@gmail.com>
2025-10-04 12:22:02 +0530

    Frontend Created.

---
+++ script.js
@@ -0,0 +1,55 @@
+const tag1 = document.querySelector('h1')
+const query = document.querySelector('.query')
+const text = ''
+
+function giveData() {
+
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+.then((res) => res.json())
+.then((res)=>
+    // showOnScreen(data)
+    showOnScreen(res)
+)
+}
+
+function showOnScreen(some) {
+    // data = data.meals4
+    let data = some.meals[0]
+        const div = document.createElement('div')
+        const image = document.createElement('img')
+        const name = document.createElement('h1')
+        const instruction = document.createElement('pre')
+        const tags = document.createElement('h3')
+        const bigdiv = document.querySelector('.hello')
+        const ingredient = document.createElement('div')
+        const ingredientHeading = document.createElement('h1')
+        console.log(data)
+        image.src  = `${data.strMealThumb}`
+        tags.textContent = `${data.strTags}`
+        name.textContent = `${data.strMeal}`
+        instruction.textContent = `${data.strInstructions}`
+        ingredientHeading.textContent = "Ingredients : "
+        ingredient.append(ingredientHeading)
+        displayIngredients(data, ingredient)
+        div.append(image)
+        div.append(name)
+        div.append(instruction)
+        div.append(tags)
+        div.append(ingredient)
+        bigdiv.appendChild(div)
+}
+
+function displayIngredients(data, ingredient) {
+    for(let i = 1; i < 20; i++) {
+           let str = `strIngredient`
+            const p = document.createElement('p')
+            str += i;
+            if(!data[str]) {
+                continue
+            }
+            p.textContent =  `${data[str]}`
+        
+        ingredient.appendChild(p)
+            str = ''
+    }
+}
